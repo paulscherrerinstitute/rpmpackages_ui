3ca598716bec6e2f5cea091b9356ea8c42c22ca1
Yannick Wernle <yannick@wernle.net>
2025-11-13 14:40:36 +0100

    use env in backend for auth

--- fastapi_backend/shared_resources/auth.py
+++ fastapi_backend/shared_resources/auth.py
@@ -1,6 +1,18 @@
+import os
 from fastapi_azure_auth import SingleTenantAzureAuthorizationCodeBearer
 
 
+def getScopes() -> dict[str, str]:
+    scope_env: str = os.getenv("RPM_PACKAGES_AUTH_SCOPES", "")
+    scopes: dict = {}
+    for single_scope in scope_env.split(";"):
+        scopes[single_scope] = ""
+    return scopes
+
+
 azure_scheme = SingleTenantAzureAuthorizationCodeBearer(
-    app_client_id="", tenant_id="", scopes={"": ""}, allow_guest_users=True
+    app_client_id=os.getenv("RPM_PACKAGES_AUTH_CLIENT_ID", ""),
+    tenant_id=os.getenv("RPM_PACKAGES_AUTH_TENANT_ID", ""),
+    scopes=getScopes(),
+    allow_guest_users=True,
 )

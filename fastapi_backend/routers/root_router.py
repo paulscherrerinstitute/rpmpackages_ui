11dded4d28ba1e129f0a2505a5c28658b9182ad1
Yannick Wernle <yannick@wernle.net>
2025-11-24 16:25:13 +0100

    log external deletions of files

--- fastapi_backend/routers/root_router.py
+++ fastapi_backend/routers/root_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from socket import gethostname
 from os import getenv
+from shared_resources.event_manager import get_all_events
 
 router = APIRouter()
 
@@ -19,3 +20,8 @@ async def get_host() -> str:
 async def get_rpm_location() -> str:
     dir: str | None = getenv("RPM_PACKAGES_DIRECTORY")
     return dir if dir is not None else ""
+
+
+@router.get("/events")
+async def get_events():
+    return get_all_events()
